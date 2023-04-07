c9d0090b1110988bce5530df9ba640a9e8915ad7
Daniel Gehrer <daniel.dev@xa1.at>
2023-04-07 18:12:21 +0200

    Build for multiple targets

--- build.sh
+++ build.sh
@@ -1,4 +1,44 @@
-rm -rf build
-mkdir -p build
+#!/bin/bash
 
-go build -o build/serve
+set -e
+
+rm -rf build
+
+# Define the target operating systems and architectures
+TARGETS=(
+    "linux/386/"
+    "linux/amd64/"
+    "linux/arm64/"
+    "darwin/amd64/"
+    "darwin/arm64/"
+    "windows/386/.exe"
+    "windows/amd64/.exe"
+)
+
+product_name="serve"
+start_dir="${PWD}"
+
+for target in "${TARGETS[@]}"; do
+    # Split the target operating system and architecture into separate variables
+    IFS='/' read -r os arch extension <<< "${target}"
+
+    # Define the output directory
+    output_dir="build/${os}_${arch}"
+    output_name="${product_name}${extension}"
+
+    # Create the output directory if it doesn't exist
+    mkdir -p "${output_dir}"
+
+    # Build the program for the target operating system and architecture
+    GOOS="${os}" GOARCH="${arch}" go build -ldflags="-s -w" -trimpath -o "${output_dir}/${output_name}" .
+
+    # Zip the executable file
+    cd "${output_dir}"
+    zip "build.zip" "${output_name}"
+    mv "build.zip" "../${product_name}_${os}_${arch}.zip"
+
+    cd "${start_dir}"
+
+    # Print a message indicating that the build was successful
+    echo "Build for ${os}/${arch} successful."
+done
